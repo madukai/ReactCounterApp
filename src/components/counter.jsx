6af8e17f05ad12009a27c397079d0c25bf42c6c4
Joji <noypako@hotmail.com>
2018-12-26 19:59:03 -0800

    Added modification to the code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.value
     };
 
     styles = {
@@ -12,57 +11,79 @@ class Counter extends Component {
         fontWeight: 600
     };
 
+    // Dynamic HTML element
     renderTags() {
         if (this.state.tags.length === 0) return <p>There are no tags!</p>
 
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     }
 
-    handleIncrement = (product) => {
-        this.setState({ count: this.state.count + 1 });
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 });
+    }
+
+    handleDecrement = () => {
+        // check
+        if (this.state.value > 0) {
+            this.setState({ value: this.state.value - 1 });
+        }
     }
 
     handleResetCounter = () => {
-        this.setState({ count: 0 });
+        this.setState({ value: 0 });
     }
 
-    // one solution: wrapper function
-    doHandldeIncrement = () => {
-        this.handleIncrement({ id: 1 });
-    };
-
     render() {
-        const product = { id: 1 };
         return (
-            <React.Fragment>
+            <div>
                 <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-2">Decrement</button>
                 <button onClick={this.handleResetCounter} className="btn btn-secondary btn-sm m-2">Reset</button>
-                {this.renderTags()}
-            </React.Fragment>
+            </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
 
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value } = this.state;
         const x = 'Zero';
-        return count === 0 ? x : count;
+        return value === 0 ? x : value;
     }
 }
 
 export default Counter;
 
 //1:17:31 Composing Components
+
  /*
-  * bind call to have access to 'this'
+  // one solution: wrapper function to pass arguments
+    doHandldeIncrement = () => {
+        this.handleIncrement({ id: 1 });
+    }; 
+
+   // bind call to have access to 'this'
     constructor() {
         super(); // constructor of Component object
         this.handleIncrement = this.handleIncrement.bind(this);
-    } */
+    } 
+
+    // calling a reference to a function and passing an arguments
+    <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
+
+    // dynamically create an html element
+    tags: ['tag1', 'tag2', 'tag3']
+    {this.renderTags()}
+
+    // adding inline style
+    style={this.styles}
+
+     // wrap react component without using div
+     <React.Fragment> </React.Fragment>
+*/
